6e2553c228a2072b29b833699900c7095be1a2a5
Santosh Shedbalkar <sanoysyg@gmail.com>
2021-06-26 03:54:35 +0530

    Committing first working version of the working TCP client and server code

---
+++ src/nw/client.cmake
@@ -0,0 +1,23 @@
+set(PROJECT_NAME mmo_client)
+project(${PROJECT_NAME} VERSION 0.1.0 LANGUAGES CXX)
+
+# Add source files
+set(CLIENT_SOURCE_FILES 
+nw/MMO_Client.cpp
+log/Logging.cpp
+thread/asyncgetline.cpp
+)
+
+# the above paths and defines will be used in this build
+add_executable(${PROJECT_NAME} WIN32 ${CLIENT_SOURCE_FILES})
+# set(RUNTIME_OUTPUT_DIRECTORY ${PROJECT_SOURCE_DIR}/bin/$<CONFIG>)
+target_include_directories(${PROJECT_NAME} PRIVATE "/usr/local/include")
+
+set_target_properties(${PROJECT_NAME}
+    PROPERTIES
+    ARCHIVE_OUTPUT_DIRECTORY "${CMAKE_SOURCE_DIR}/bin/$<CONFIG>/lib"
+    LIBRARY_OUTPUT_DIRECTORY "${CMAKE_SOURCE_DIR}/bin/$<CONFIG>/lib"
+    RUNTIME_OUTPUT_DIRECTORY "${CMAKE_SOURCE_DIR}/bin/$<CONFIG>/bin"
+)
+
+target_include_directories(${PROJECT_NAME} PRIVATE ${CMAKE_SOURCE_DIR}/include)
